7b8f07acb19a737780d5a9ae2aff2025328bb614
ilcheong <ilcheong0123@gmail.com>
2025-05-31 17:28:10 +0900

    new file:   add.js 	new file:   data.js 	new file:   index.html 	new file:   style.css

---
+++ data.js
@@ -0,0 +1,33 @@
+const articles = [
+    {
+        id:1,
+        title: 'C. Marc-André ter Stegen',
+        date: new Date(1992,4,30),
+        length: 11,
+        snippet: 'Goalkeeper who won the Champions League with Barcelona'
+    },
+
+    {
+        id:2,
+        title: 'VC. Ronald Federico Araújo da Silva',
+        date: new Date(1999,3,7),
+        length: 11,
+        snippet: 'Center back from Uruguay'
+    },
+
+    {
+        id:3,
+        title: '3C. Frenkie de Jong',
+        date: new Date(1997,5,12),
+        length: 11,
+        snippet: 'Starting midfielder in his 7th season'
+    },
+
+    {
+        id:4,
+        title: '4C. Raphael Dias Belloli',
+        date: new Date(1996,12,14),
+        length: 11,
+        snippet: "Ballon d'Or candidate who terrifies the defense"
+    },
+];
